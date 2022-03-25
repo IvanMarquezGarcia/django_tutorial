57ba169e0f4f8b5d4d1d5fd2a1f44d112ad55c95
Iván <ivanmarquezgarcia00@gmail.com>
2022-03-25 11:32:41 +0100

    Agregado formulario de votación

--- mysite/encuestas/urls.py
+++ mysite/encuestas/urls.py
@@ -5,8 +5,8 @@ from . import views	# importar vistas de la app
 app_name = 'encuestas'
 
 urlpatterns = [
-	path('', views.inicio, name = 'inicio'), 	# esto debería estar en la app de login, pero no tengo
-	path('inicio/', views.inicio, name = 'inicio'), # esto debería estar en la app de login, pero no tengo
+	path('', views.portal, name = 'portal'), 	# esto debería estar en la app de login, pero no tengo
+	path('portal/', views.portal, name = 'portal'), # esto debería estar en la app de login, pero no tengo
 	path('encuestas/', views.encuestas, name = 'encuestas'),
 	path('encuestas/<int:id_pregunta>/', views.detalles, name = 'detalles'),
 	path('encuestas/<int:id_pregunta>/resultados/', views.resultados, name = 'resultados'),
--- mysite/encuestas/views.py
+++ mysite/encuestas/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from encuestas.models import Pregunta
+from django.urls import reverse
+
+from encuestas.models import Pregunta, Eleccion
+
+from django.db.models import F
 
 # Create your views here.
-def inicio(request):
+def portal(request):
 	ultimas_cinco_preguntas = Pregunta.objects.order_by('-fec_pub')[:5]
 	context = {'ultimas_cinco_preguntas': ultimas_cinco_preguntas}
-	return render(request, 'encuestas/inicio.html', context)
+	return render(request, 'encuestas/portal.html', context)
 '''
 	output = "<h1>Inicio del sitio</h1><br><br>"
 	output += "Estas son las cinco últimas preguntas:<br>"
@@ -20,7 +24,10 @@ def inicio(request):
 	return HttpResponse(output)
 '''
 def encuestas(request):
-	return HttpResponse("Inicio de encuestas")
+	ultimas_cinco_preguntas = Pregunta.objects.order_by('-fec_pub')[:5]
+	context = {'ultimas_cinco_preguntas': ultimas_cinco_preguntas}
+	return render(request, 'encuestas/inicio.html', context)
+#	return HttpResponse("Inicio de encuestas")
 
 def detalles(request, id_pregunta):
 	pregunta = get_object_or_404(Pregunta, pk = id_pregunta)
@@ -28,18 +35,20 @@ def detalles(request, id_pregunta):
 	#return HttpResponse("Detalles de pregunta " + str(id_pregunta))
 
 def resultados(request, id_pregunta):
-	return HttpResponse("Resultados de pregunta " + str(id_pregunta))
+	pregunta = get_object_or_404(Pregunta, pk=id_pregunta)
+	return render(request, 'encuestas/resultados.html', {'pregunta': pregunta})
+#	return HttpResponse("Resultados de pregunta " + str(id_pregunta))
 
 def votar(request, id_pregunta):
 	pregunta = get_object_or_404(Pregunta, pk = id_pregunta)
 
 	try:
 		selected_eleccion = pregunta.eleccion_set.get(pk = request.POST['eleccion'])
-	except (KeyError, Eleccion.DoesNotExit):
+	except (KeyError, Eleccion.DoesNotExist):
 		# volver a mostrar el formulario de la pregunta
 		return render(request, 'encuestas/detalles.html', {'pregunta': pregunta, 'msg_err': "No has seleccionado ninguna opción"})
 	else:
-		selected_eleccion.votos += 1
+		selected_eleccion.votos = F('votos') + 1
 		selected_eleccion.save()
 		return HttpResponseRedirect(reverse('encuestas:resultados', args=(pregunta.id,)))
 
